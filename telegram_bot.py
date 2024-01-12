fed8492dd282428572fbc829b0721f0b7c04f658
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2024-01-12 14:57:45 +0300

    Structured work with the logger

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,58 +1,43 @@
 import logging
 import os
-from dotenv import load_dotenv
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+from functools import partial
+from dotenv import load_dotenv
+from telegram import Update, Bot
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+
+from logger import TelegramLogHandler
+from vk_bot import detect_intent_texts
+
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Здравствуйте, {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    flowresponse = detect_intent_texts(update.message.text)
-    update.message.reply_text(flowresponse)
-
-
-def detect_intent_texts(text, language_code="ru"):
-    """Returns the result of detect intent with texts as inputs."""
-    from google.cloud import dialogflow
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(os.environ["PROJECT_ID"], os.environ["PROJECT_ID"])
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
+def generic_response(update: Update, context: CallbackContext, project_id) -> None:
+    session_id = update.effective_chat.id
+    flow_response = detect_intent_texts(project_id, session_id, update.message.text)
+    update.message.reply_text(flow_response.query_result.fulfillment_text)
 
 
 def main() -> None:
-    load_dotenv()
-    telegram_token = os.environ["TG_TOKEN"]
-    """Start the bot."""
-    updater = Updater(telegram_token)
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
 
+    load_dotenv()
+    telegram_token_log = os.environ["TG_TOKEN"]
+    dialogflow_project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+    tg_chat_id_log = os.environ["TG_CHAT_ID"]
+
+    bot = Bot(token=telegram_token_log)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogHandler(bot, tg_chat_id_log))
+
+    updater = Updater(telegram_token_log)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    send_response_with_project_id = partial(generic_response, project_id=dialogflow_project_id)
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, send_response_with_project_id))
     updater.start_polling()
+
     updater.idle()
 
 
--- vk_bot.py
+++ vk_bot.py
@@ -1,41 +1,52 @@
+import logging
 import os
 import random
 import vk_api as vk
+
 from dotenv import load_dotenv
+from telegram import Bot
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from detect_intent import detect_intent_texts
+from logger import TelegramLogHandler
 
-def detect_intent_texts(text, language_code="ru"):
-    """Returns the result of detect intent with texts as inputs.
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-    from google.cloud import dialogflow
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(os.environ["PROJECT_ID"], os.environ["PROJECT_ID"])
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
-    return "" if response.query_result.intent.is_fallback else response.query_result.fulfillment_text
+logger = logging.getLogger(__name__)
 
 
-def echo(event, vk_api):
-    text = detect_intent_texts(event.text)
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=text,
-        random_id=random.randint(1, 1000)
-    )
+def generic_response(event, vk_api, project_id):
+    user_id = event.user_id
+    message = event.text
+    flow_response = detect_intent_texts(project_id=project_id,
+                                        session_id=user_id,
+                                        text=message)
+    if not flow_response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=flow_response.query_result.fulfillment_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    )
+
     load_dotenv()
-    vk_session = vk.VkApi(token=os.environ['VK_TOKEN'])
+
+    vk_token = os.environ['VK_TOKEN']
+    telegram_token_logs = os.environ['TG_TOKEN']
+    tg_chat_id = os.environ['TG_CHAT_ID']
+
+    dialogflow_project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+
+    bot = Bot(token=telegram_token_logs)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogHandler(bot, tg_chat_id))
+
+    vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            generic_response(event, vk_api, dialogflow_project_id)
